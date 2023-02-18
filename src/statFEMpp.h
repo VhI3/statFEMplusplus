49d3112b69dbccad644c7d461cbf8034ecf1ff3b
VhI3 <48403210+VhI3@users.noreply.github.com>
2023-02-18 23:58:32 +0100

    change structure

---
+++ src/statFEMpp.h
@@ -0,0 +1,10 @@
+﻿// statFEMPP.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Core>
+
+// TODO: Reference additional headers your program requires here.
