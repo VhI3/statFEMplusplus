ca5d9971d6ffa11d85671f18d9502dff865c8b45
VhI3 <48403210+VhI3@users.noreply.github.com>
2023-02-19 22:51:46 +0100

    vscode settings

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,13 +1,13 @@
 ﻿#include "statFEMpp.h"
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 
     // The test of lagrange 1D
-	int p = 2;
-	auto xi = -0.2;
-	Eigen::VectorXd rangeNaturalCoordinate = Eigen::VectorXd::LinSpaced(static_cast<Eigen::Index>(2), 0, 1);
+    int p = 2;
+    auto xi = -0.2f;
+    Eigen::VectorXd rangeNaturalCoordinate = Eigen::VectorXd::LinSpaced(static_cast<Eigen::Index>(2), 0, 1);
     std::cout << rangeNaturalCoordinate << std::endl;
 
     return 0;
