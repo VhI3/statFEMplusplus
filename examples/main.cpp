53874cf33889a06ad48da84afd176b7f508749bb
VhI3 <48403210+VhI3@users.noreply.github.com>
2023-03-05 14:41:18 +0100

    auto integration

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char **argv)
 {
 
     // The test of lagrange 1D
-    int p = 2;
-    auto xi = -0.2f;
+    auto p { 2 };
+    auto xi { -0.2f } ;
     Eigen::VectorXd rangeNaturalCoordinate = Eigen::VectorXd::LinSpaced(static_cast<Eigen::Index>(2), 0, 1);
     std::cout << rangeNaturalCoordinate << std::endl;
 
