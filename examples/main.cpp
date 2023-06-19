fb859f7bc0b5f634e26cdb9a06df6d1783652414
VhI3 <48403210+VhI3@users.noreply.github.com>
2023-06-19 22:32:15 +0200

    update

--- examples/main.cpp
+++ examples/main.cpp
@@ -6,9 +6,15 @@ int main(int argc, char **argv)
 {
 
     // We fist define the geometry of domain. For geometry we need first Points and then Lines and then Surfaces and then Volumes. 
-    Point<double> p1({0, 0, 0});
-    Point<double> p2({1, 0, 0});
-    // std::cout << p1.getCoordinates() << std::endl;
+    auto p1 = Point<double> ({0, 0, 0});
+    auto p2 = Point<double> ({1, 0, 0});
+
+
+    auto l1 = line(p1, p2);
+
+
+    // auto p3 = Point<double> ({2, 0, 0});
+    // std::cout << p3.getCoordinates() << std::endl;
 
     // p1.setCoordinates({1, 0, 0});
 
---
+++ src/Line.h
@@ -0,0 +1,19 @@
+//
+// Created by vahab on 3/30/23.
+//
+// The decleration of the classes is performed in a *.h file
+#pragma once
+#include <Eigen/Dense>
+#include <Eigen/Core>
+template <class T>
+class Line 
+{
+public:
+    explicit Line(const Eigen::Vector<T, 3> &coordinates);
+
+    const Eigen::Vector<T, 3> &getCoordinates() const;
+    void setCoordinates(const Eigen::Vector<T, 3> &coordinates);
+
+private:
+    Eigen::Vector<T, 3> m_coordinates;
+};
