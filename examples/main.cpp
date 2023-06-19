e6ed4224f056d3c486a18eb83b6287561e765045
VhI3 <48403210+VhI3@users.noreply.github.com>
2023-06-19 22:32:00 +0200

    update

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,20 +5,14 @@
 int main(int argc, char **argv)
 {
 
-    // The test of lagrange 1D
-    //    const auto p{2};
-    //    const auto xi{-0.2f};
-    //    auto rangeNaturalCoordinate = Eigen::VectorXd::LinSpaced(static_cast<Eigen::Index>(2), 0, 1);
-    //    std::cout << rangeNaturalCoordinate.transpose() << std::endl;
+    // We fist define the geometry of domain. For geometry we need first Points and then Lines and then Surfaces and then Volumes. 
+    Point<double> p1({0, 0, 0});
+    Point<double> p2({1, 0, 0});
+    // std::cout << p1.getCoordinates() << std::endl;
 
-    //    Num n(35);
-    //    std::cout << n.getNum() << std::endl;
-    Point<double> myNode({0, 0, 0});
-    std::cout << myNode.getCoordinates() << std::endl;
+    // p1.setCoordinates({1, 0, 0});
 
-    myNode.setCoordinates({1, 0, 0});
-
-    std::cout << myNode.getCoordinates() << std::endl;
+    // std::cout << p1.getCoordinates() << std::endl;
 
     return 0;
 }
