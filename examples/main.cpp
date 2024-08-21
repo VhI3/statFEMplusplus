94a2877b52a710fa1b94171f0ed740c20bfeb386
VhI3 <48403210+VhI3@users.noreply.github.com>
2024-08-21 20:06:01 +0200

    update

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,7 +1,34 @@
 ﻿
 // #include "matplot/matplot.h"
+#include "exprtk.hpp"
 #include "statFEMplusplus.hpp"
 #include <cmath>
+
+template <typename T> void trig_function() {
+  typedef exprtk::symbol_table<T> symbol_table_t;
+  typedef exprtk::expression<T> expression_t;
+  typedef exprtk::parser<T> parser_t;
+
+  const std::string expression_string =
+      "clamp(-1.0,sin(2 * pi * x) + cos(x / 2 * pi),+1.0)";
+
+  T x;
+
+  symbol_table_t symbol_table;
+  symbol_table.add_variable("x", x);
+  symbol_table.add_constants();
+
+  expression_t expression;
+  expression.register_symbol_table(symbol_table);
+
+  parser_t parser;
+  parser.compile(expression_string, expression);
+
+  for (x = T(-5); x <= T(+5); x += T(0.001)) {
+    const T y = expression.value();
+    printf("%19.15f\t%19.15f\n", x, y);
+  }
+}
 
 int main(int argc, char **argv) {
 
@@ -27,5 +54,11 @@ int main(int argc, char **argv) {
   // //
   // show();
   // //
+  //
+  //
+  //
+  //
+  trig_function<double>();
+
   return 0;
 }
