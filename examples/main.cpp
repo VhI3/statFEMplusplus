c5963fcdaad00fbb87d54cc0d9b45ddfe9576636
VhI3 <48403210+VhI3@users.noreply.github.com>
2024-08-29 17:35:20 +0200

    clean main

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,39 +1,8 @@
 ﻿
-#include "exprtk.hpp"
-#include "matplot/freestanding/plot.h"
-#include "matplot/matplot.h"
 #include "statFEMplusplus.hpp"
 #include <cmath>
 
-template <typename T> void trig_function() {
-  typedef exprtk::symbol_table<T> symbol_table_t;
-  typedef exprtk::expression<T> expression_t;
-  typedef exprtk::parser<T> parser_t;
-
-  const std::string expression_string =
-      "clamp(-1.0,sin(2 * pi * x) + cos(x / 2 * pi),+1.0)";
-
-  T x;
-
-  symbol_table_t symbol_table;
-  symbol_table.add_variable("x", x);
-  symbol_table.add_constants();
-
-  expression_t expression;
-  expression.register_symbol_table(symbol_table);
-
-  parser_t parser;
-  parser.compile(expression_string, expression);
-
-  for (x = T(-5); x <= T(+5); x += T(0.001)) {
-    const T y = expression.value();
-    printf("%19.15f\t%19.15f\n", x, y);
-  }
-}
-
 int main(int argc, char **argv) {
-
-  using namespace matplot;
 
   Node<double> myNode({1., 2, 3});
   std::cout << myNode.getPosition() << std::endl;
@@ -41,49 +10,8 @@ int main(int argc, char **argv) {
   myNode.setPosition({1, 22, 33});
   std::cout << myNode.getPosition() << std::endl;
 
-  std::cout << "Test windows" << std::endl;
-
   // Create nodes 1 to 3
   Node<double> node1({1, 2, 3}), node2({4, 5, 6}), node3({7, 8, 9});
-
-  /*std::vector<double> x = linspace(0, 2 * pi);*/
-  /*std::vector<double> y1 = transform(x, [](auto x) { return sin(x); });*/
-
-  /*plot(x, y1);*/
-  /*show();*/
-
-  const std::string f_x_exp = "25x^5 - 35x^4 - 15x^3 + 40x^2 - 15x + 1";
-  exprtk::symbol_table<double> symbol_table;
-  exprtk::parser<double> parser;
-  exprtk::expression<double> f_x;
-
-  std::vector<double> x = linspace(0, 1.0);
-  // Print all the elements of x
-  for (int i = 0; i < x.size(); i++) {
-    std::cout << x[i] << std::endl;
-  }
-
-  symbol_table.add_variable("x", x[0]);
-  f_x.register_symbol_table(symbol_table);
-
-  parser.compile(f_x_exp, f_x);
-
-  /*// Store the result*/
-  /*std::vector<double> y(x.size());*/
-  /*for (int i = 0; i < x.size(); i++) {*/
-  /*  y[i] = f_x.value();*/
-  /*  symbol_table.add_variable("x", x[i]);*/
-  /*}*/
-
-  // Define y1 as a lambda function
-  auto y1 = transform(x, [](auto x) { return f_x.value(); });
-  // but it can not be parsed as lambda
-  /*std::vector<double> y1 = transform(x, (auto x) { return f_x.value(); });*/
-  /*std::cout << f_x_exp << " = " << f_x.value() << std::endl;*/
-  plot(x, y1);
-  show();
-
-  /*trig_function<double>();*/
 
   return 0;
 }
--- examples/main_exprtk_matplot.cpp
+++ examples/main_exprtk_matplot.cpp
@@ -1,4 +1,3 @@
-#include "matplot/freestanding/axes_functions.h"
 #include <statFEMplusplus.hpp>
 
 int main(int argc, char **argv) {
@@ -8,7 +7,6 @@ int main(int argc, char **argv) {
   exprtk::symbol_table<double> symbol_table;
   exprtk::expression<double> f_x;
   exprtk::parser<double> parser;
-
   double x_var; // Variable to hold the value of x
   symbol_table.add_variable("x", x_var);
   f_x.register_symbol_table(symbol_table);
@@ -17,17 +15,14 @@ int main(int argc, char **argv) {
     std::cerr << "Error: " << parser.error() << "\n";
     return 1;
   }
-
   // Define the range for x
-  std::vector<double> x = linspace(0.0, 1.0, 100); // 100 points between 0 and 1
-  std::vector<double> y;
-
+  std::vector<double> x = linspace(0.0, 1.0, 100); // 100 points between 0
   // Evaluate the expression for each x
+  std::vector<double> y;
   for (double xi : x) {
     x_var = xi;               // Set the variable x to the current value
     y.push_back(f_x.value()); // Evaluate f_x at the current value of x
   }
-
   // Plot the result
   xlabel("x");
   ylabel("f(x)");
@@ -37,6 +32,10 @@ int main(int argc, char **argv) {
   plot(x, y);
   show();
 
+  // std::vector<double> x = linspace(0, 2 * pi);
+  // std::vector<double> y1 = transform(x, [](auto x) { return sin(x); });
+  // plot(x, y1);
+  // show();
+  //
   return 0;
 }
-
